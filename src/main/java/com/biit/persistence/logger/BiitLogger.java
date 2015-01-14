d612d90d67b497041437e77053c8f3102cf1b192
aperez <aperez@biit-solutions.com>
2015-01-14 09:36:54 +0100

    Removed the call throwable.printstacktrace(...) and replaced by a stack trace printer

--- src/main/java/com/biit/persistence/logger/BiitLogger.java
+++ src/main/java/com/biit/persistence/logger/BiitLogger.java
@@ -1,9 +1,5 @@
 package com.biit.persistence.logger;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.Writer;
-
 import org.apache.log4j.Logger;
 
 /**
@@ -16,8 +12,9 @@ public class BiitLogger {
 	}
 
 	/**
-	 * Events that have business meaning (i.e. creating category, deleting form, ...). To follow user actions.
-	 *
+	 * Events that have business meaning (i.e. creating category, deleting form,
+	 * ...). To follow user actions.
+	 * 
 	 * @param message
 	 */
 	private static void info(String message) {
@@ -25,15 +22,17 @@ public class BiitLogger {
 	}
 
 	/**
-	 * Events that have business meaning (i.e. creating category, deleting form, ...). To follow user actions.
+	 * Events that have business meaning (i.e. creating category, deleting form,
+	 * ...). To follow user actions.
 	 */
 	public static void info(String className, String message) {
 		info(className + ": " + message);
 	}
 
 	/**
-	 * Shows not critical errors. I.e. Email address not found, permissions not allowed for this user, ...
-	 *
+	 * Shows not critical errors. I.e. Email address not found, permissions not
+	 * allowed for this user, ...
+	 * 
 	 * @param message
 	 */
 	private static void warning(String message) {
@@ -41,8 +40,9 @@ public class BiitLogger {
 	}
 
 	/**
-	 * Shows not critical errors. I.e. Email address not found, permissions not allowed for this user, ...
-	 *
+	 * Shows not critical errors. I.e. Email address not found, permissions not
+	 * allowed for this user, ...
+	 * 
 	 * @param message
 	 */
 	public static void warning(String className, String message) {
@@ -50,9 +50,9 @@ public class BiitLogger {
 	}
 
 	/**
-	 * For following the trace of the execution. I.e. Knowing if the application access to a method, opening database
-	 * connection, etc.
-	 *
+	 * For following the trace of the execution. I.e. Knowing if the application
+	 * access to a method, opening database connection, etc.
+	 * 
 	 * @param message
 	 */
 	private static void debug(String message) {
@@ -62,17 +62,17 @@ public class BiitLogger {
 	}
 
 	/**
-	 * For following the trace of the execution. I.e. Knowing if the application access to a method, opening database
-	 * connection, etc.
+	 * For following the trace of the execution. I.e. Knowing if the application
+	 * access to a method, opening database connection, etc.
 	 */
 	public static void debug(String className, String message) {
 		debug(className + ": " + message);
 	}
 
 	/**
-	 * To log any not expected error that can cause application malfuncionality. I.e. couldn't open database connection,
-	 * etc..
-	 *
+	 * To log any not expected error that can cause application malfuncionality.
+	 * I.e. couldn't open database connection, etc..
+	 * 
 	 * @param message
 	 */
 	private static void severe(String message) {
@@ -81,7 +81,7 @@ public class BiitLogger {
 
 	/**
 	 * To log any not expected error that can cause application malfuncionality.
-	 *
+	 * 
 	 * @param message
 	 */
 	public static void severe(String className, String message) {
@@ -90,7 +90,7 @@ public class BiitLogger {
 
 	/**
 	 * Used for debugging when accessing to a method.
-	 *
+	 * 
 	 * @param className
 	 * @param method
 	 */
@@ -100,7 +100,7 @@ public class BiitLogger {
 
 	/**
 	 * Used for debugging when exiting from a method.
-	 *
+	 * 
 	 * @param className
 	 * @param method
 	 */
@@ -110,20 +110,22 @@ public class BiitLogger {
 
 	/**
 	 * To log java exceptions and log also the stack trace.
-	 *
+	 * 
 	 * @param className
 	 * @param throwable
 	 */
 	public static void errorMessage(String className, Throwable throwable) {
-		String error = getStackTrace(throwable);
+		String error = stackTraceToString(throwable);
 		severe(className, error);
 	}
 
-	private static String getStackTrace(Throwable throwable) {
-		Writer writer = new StringWriter();
-		PrintWriter printWriter = new PrintWriter(writer);
-		throwable.printStackTrace(printWriter);
-		return writer.toString();
+	public static String stackTraceToString(Throwable e) {
+		StringBuilder sb = new StringBuilder();
+		for (StackTraceElement element : e.getStackTrace()) {
+			sb.append(element.toString());
+			sb.append("\n");
+		}
+		return sb.toString();
 	}
 
 	public static boolean isDebugEnabled() {
