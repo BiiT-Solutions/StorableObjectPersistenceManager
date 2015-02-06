c95627846326b87f5985c01ac80a6422598e7eb7
Jorge Hortelano <jhortelano@biit-solutions.com>
2015-02-06 16:31:02 +0100

    New Exception.

---
+++ src/main/java/com/biit/persistence/entity/exceptions/InvalidNameException.java
@@ -0,0 +1,9 @@
+package com.biit.persistence.entity.exceptions;
+
+public class InvalidNameException extends Exception {
+	private static final long serialVersionUID = 8192681412925790911L;
+
+	public InvalidNameException(String message) {
+		super(message);
+	}
+}
