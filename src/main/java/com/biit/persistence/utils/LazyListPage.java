41b3fe03f8827cd09c828cf685320a40acb34534
Jorge Hortelano <jhortelano@biit-solutions.com>
2022-04-04 11:01:14 +0200

    Corrected spotbugs

--- src/main/java/com/biit/persistence/utils/LazyListPage.java
+++ src/main/java/com/biit/persistence/utils/LazyListPage.java
@@ -1,32 +1,33 @@
 package com.biit.persistence.utils;
 
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.List;
 
-public class LazyListPage<T> implements Comparable<LazyListPage<?>> {
+public class LazyListPage<T> implements Comparable<LazyListPage<?>>, Serializable {
 
-	private final int pageNumber;
-	private final List<T> content;
+    private final int pageNumber;
+    private final List<T> content;
 
-	public LazyListPage(int pageNumber, List<T> content) {
-		this.pageNumber = pageNumber;
-		this.content = Collections.unmodifiableList(content);
-	}
+    public LazyListPage(int pageNumber, List<T> content) {
+        this.pageNumber = pageNumber;
+        this.content = Collections.unmodifiableList(content);
+    }
 
-	public int getPageNumber() {
-		return pageNumber;
-	}
+    public int getPageNumber() {
+        return pageNumber;
+    }
 
-	@Override
-	public int compareTo(LazyListPage<?> o) {
-		return Integer.compare(pageNumber, o.getPageNumber());
-	}
+    @Override
+    public int compareTo(LazyListPage<?> o) {
+        return Integer.compare(pageNumber, o.getPageNumber());
+    }
 
-	public T get(int indexOnPage) {
-		return content.get(indexOnPage);
-	}
+    public T get(int indexOnPage) {
+        return content.get(indexOnPage);
+    }
 
-	public List<T> getContent() {
-		return content;
-	}
+    public List<T> getContent() {
+        return content;
+    }
 }
